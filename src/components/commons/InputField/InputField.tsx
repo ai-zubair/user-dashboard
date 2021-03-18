476b65f22a2b72d019fa100a3a58bb1c492d59ab
Zubair Bashir <zubair.bashirzubair@gmail.com>
2021-03-18 23:00:14 +0530

    Input Field a controlled component

--- src/components/commons/InputField/InputField.tsx
+++ src/components/commons/InputField/InputField.tsx
@@ -1,19 +1,26 @@
-import React, { FunctionComponent } from 'react';
+import React, { ChangeEvent, FunctionComponent } from 'react';
 import { InputFieldWrapper } from './InputFieldStyled';
 
 interface InputFieldProps{
   fieldLabel: string;
   fieldID: string;
   fieldType: InputFieldType;
+  fieldValue: string;
+  onFieldChange:(changedValue: string)=>void;
 }
 
 export type InputFieldType = "text" | "password";
 
-const InputField: FunctionComponent<InputFieldProps> = ({fieldLabel, fieldID, fieldType}) => {
+const InputField: FunctionComponent<InputFieldProps> = ({fieldLabel, fieldID, fieldType, fieldValue, onFieldChange}) => {
   return (
     <InputFieldWrapper>
       <label htmlFor={fieldID}>{fieldLabel}</label>
-      <input id={fieldID} type={fieldType}/>
+      <input 
+        id={fieldID} 
+        type={fieldType} 
+        value={fieldValue} 
+        onChange={(event: ChangeEvent<HTMLInputElement>) => onFieldChange(event.target.value)}
+      />
     </InputFieldWrapper>
   )
 }
