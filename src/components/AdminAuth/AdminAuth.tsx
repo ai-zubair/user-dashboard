0bd7dba4407816f84a5be9853cc0bb2b3c48d34e
Zubair Bashir <zubair.bashirzubair@gmail.com>
2021-03-19 04:10:24 +0530

    Remove white spaces

--- src/components/AdminAuth/AdminAuth.tsx
+++ src/components/AdminAuth/AdminAuth.tsx
@@ -10,44 +10,41 @@ import { mapStateToProps, mapDispatchToProps } from './storeMappers';
 
 
 const AdminAuth:FunctionComponent<AdminAuthProps> = (props) => {
-
   const { username, password, loginError, setUserName, setPassword, isSubmitLoaderVisible, postLoginData} = props;
-
   const { USERNAME, PASSWORD, SUBMIT } = FORM_FIELDS;
-
-  const isSubmitButtonDisabled = !USERNAME.validator(username) || !PASSWORD.validator(password) || isSubmitLoaderVisible;
+  const isLoginButtonDisabled = !USERNAME.validator(username) || !PASSWORD.validator(password) || isSubmitLoaderVisible;
 
   return (
     <Fragment>
       <Header />
-        <AdminAuthWrapper>
-          <Form 
-            submitButtonText={SUBMIT.label}
-            submitErrorMessage={loginError}
-            isSubmitButtonDisabled={isSubmitButtonDisabled}
-            showSubmitLoader={isSubmitLoaderVisible} 
-            onSubmit={()=>{postLoginData({username,password})}} 
-          >
-            <InputFormField 
-              fieldID={USERNAME.id} 
-              fieldLabel={USERNAME.label} 
-              fieldType={"text"}
-              fieldValue={username} 
-              errorMessage={USERNAME.errorMessage}
-              validator={USERNAME.validator}
-              onFieldChange={setUserName} 
-            />
-            <InputFormField 
-              fieldID={PASSWORD.id} 
-              fieldLabel={PASSWORD.label} 
-              fieldType={"password"}
-              fieldValue={password} 
-              errorMessage={PASSWORD.errorMessage}
-              validator={PASSWORD.validator}
-              onFieldChange={setPassword} 
-            />
-          </Form>
-        </AdminAuthWrapper>
+      <AdminAuthWrapper>
+        <Form 
+          submitButtonText={SUBMIT.label}
+          submitErrorMessage={loginError}
+          isSubmitButtonDisabled={isLoginButtonDisabled}
+          showSubmitLoader={isSubmitLoaderVisible} 
+          onSubmit={()=>{postLoginData({username,password})}} 
+        >
+          <InputFormField 
+            fieldID={USERNAME.id} 
+            fieldLabel={USERNAME.label} 
+            fieldType={"text"}
+            fieldValue={username} 
+            errorMessage={USERNAME.errorMessage}
+            validator={USERNAME.validator}
+            onFieldChange={setUserName} 
+          />
+          <InputFormField 
+            fieldID={PASSWORD.id} 
+            fieldLabel={PASSWORD.label} 
+            fieldType={"password"}
+            fieldValue={password} 
+            errorMessage={PASSWORD.errorMessage}
+            validator={PASSWORD.validator}
+            onFieldChange={setPassword} 
+          />
+        </Form>
+      </AdminAuthWrapper>
     </Fragment>
   )
 }
