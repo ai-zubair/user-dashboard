148fc5a35490d02a37f9b3abc28c08e26f94e332
Zubair Bashir <zubair.bashirzubair@gmail.com>
2021-03-19 02:21:14 +0530

    Add loader configuration

--- src/components/commons/Button/Button.tsx
+++ src/components/commons/Button/Button.tsx
@@ -1,17 +1,24 @@
 import React,{FunctionComponent, ReactChild} from 'react';
+import { Spinner } from '../Spinner';
 import { ButtonWrapper } from './ButtonStyled';
 
 interface ButtonProps{
   children?: ReactChild[];
   buttonText?: string;
   isButtonDisabled?: boolean; 
+  showLoader?: boolean;
 }
 
-const Button: FunctionComponent<ButtonProps> = ({buttonText, isButtonDisabled = false, children}) => {
+const Button: FunctionComponent<ButtonProps> = ({buttonText, isButtonDisabled = false, showLoader = false, children}) => {
   return (
     <ButtonWrapper isButtonDisabled={isButtonDisabled}>
-      {buttonText}
+      {showLoader ? null : buttonText}
       {children}
+      <Spinner 
+        isHidden={!showLoader} 
+        variant="small" 
+        color="var(--primary-background-color)"
+      />
     </ButtonWrapper>
   )
 }
--- src/components/commons/Button/ButtonStyled.ts
+++ src/components/commons/Button/ButtonStyled.ts
@@ -1,18 +1,21 @@
 import styled from "styled-components";
 
 export const ButtonWrapper = styled.button<{isButtonDisabled: boolean}>`
-  color: ${props => props.isButtonDisabled ? 'var(--primary-text-color)' : 'white' };
+  color: ${props => props.isButtonDisabled ? 'var(--disabled-text-color)' : 'white' };
   background-color: ${props => props.isButtonDisabled ? 'transparent' : 'var(--primary-background-color)' };
-  border: ${props => props.isButtonDisabled ? '2px solid var(--primary-text-color)' : '2px solid var(--primary-background-color)' };
+  border: ${props => props.isButtonDisabled ? '2px solid var(--disabled-text-color)' : '2px solid var(--primary-background-color)' };
   box-shadow: ${props => props.isButtonDisabled ? 'none' : 'var(--container-box-shadow)' };
-  opacity: ${props => props.isButtonDisabled ? '0.3' : '1' };
   outline: none;
   padding: 10px;
   border-radius: 4px;
   display: flex;
   flex-direction: row;
   align-items: center;
+  max-height: 43px;
   justify-content: space-around;
+  & > * {
+    transition: flex-grow,flex-shrink 0.2s ease;
+  }
   &:hover{
     box-shadow: none;
     cursor: ${props => props.isButtonDisabled ? 'not-allowed' : 'pointer' };
