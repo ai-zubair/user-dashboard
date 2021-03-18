df13b1fbd2643cca77b2de725e431fa6c306f19b
Zubair Bashir <zubair.bashirzubair@gmail.com>
2021-03-18 19:35:23 +0530

    Form styles change

--- src/components/commons/DataForm/FormStyled.ts
+++ src/components/commons/DataForm/FormStyled.ts
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const FormWrapper = styled.form`
-  padding: 10px;
+  padding: 25px;
   display: flex;
   flex-direction: column;
+  background-color: #fefefe;
   & > *{
-    margin-bottom: 10px;
+    margin-bottom: 30px;
     &:nth-last-child(1){
       margin-bottom: 0px;
     }
