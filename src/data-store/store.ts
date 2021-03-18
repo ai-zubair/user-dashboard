4222559f504e9bcb270f30c5131d92ed1c293850
Zubair Bashir <zubair.bashirzubair@gmail.com>
2021-03-19 03:13:46 +0530

    Integrate redux thunks

--- src/data-store/store.ts
+++ src/data-store/store.ts
@@ -1,10 +1,11 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { rootReducer } from './rootReducer';
+import thunk from 'redux-thunk';
 
 const reduxDevTools = typeof window !== 'undefined' && 
                       (window as any).__REDUX_DEVTOOLS_EXTENSION__ && 
                       (window as any).__REDUX_DEVTOOLS_EXTENSION__();
 
-const appDataStore = createStore(rootReducer, compose(reduxDevTools));
+const appDataStore = createStore(rootReducer, compose(applyMiddleware(thunk),reduxDevTools));
 
 export { appDataStore };
